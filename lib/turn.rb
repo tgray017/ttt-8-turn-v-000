98d2c6becc4b284d5c98edd47f01c45d600f5799
tgray017@gmail.com <tgray017@gmail.com>
2018-09-08 19:54:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,10 @@ def valid_move?(board, index)
   end  
 end  
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end

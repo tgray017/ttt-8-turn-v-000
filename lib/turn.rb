0e8003a0f1b1efe2ce804573a0b71c77a4514d85
tgray017@gmail.com <tgray017@gmail.com>
2018-09-08 19:54:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,12 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end  
+end  
+
